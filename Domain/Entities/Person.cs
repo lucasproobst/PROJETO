0af4d7080eeb1e44c30e808a6d7ccc259558895d
Lucas Probst <81838361+lucasproobst@users.noreply.github.com>
2023-05-15 18:18:46 -0300

    Signed-off-by: Lucas Probst <81838361+lucasproobst@users.noreply.github.com>

---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,9 @@
+namespace Domain.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Idade { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Data.Context;
+using Domain.Entities;
+
+var db = new MeuContexto();
+
+// Create people
+Console.WriteLine("Inserting a new person");
+var person = new Person { Id = 1, Nome = "João da Silva", Idade = DateTime.Now };
+db.Add(person);
+db.SaveChanges();
